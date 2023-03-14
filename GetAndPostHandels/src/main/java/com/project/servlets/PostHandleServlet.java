1028f57f998a7d821a88e0abde419fee76f89eab
Manoj M <manumadhu1425@gmail.com>
2023-03-14 22:06:36 +0530

    Assisted practice : GET and POST

---
+++ GetAndPostHandels/src/main/java/com/project/servlets/PostHandleServlet.java
@@ -0,0 +1,25 @@
+package com.project.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/postRequest")
+public class PostHandleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		
+		PrintWriter out = response.getWriter();
+		out.println("Firstname: "+firstname);
+		out.println("Lastname: "+lastname);
+	}
+}
